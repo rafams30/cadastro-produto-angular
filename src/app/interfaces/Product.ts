69ea766415aaea38881925a7b8aea12f0e7b599f
Rafael Martines <rafams_22@hotmail.com>
2024-07-11 20:26:06 -0300

    Criando as interfaces Product e Category

---
+++ src/app/interfaces/Product.ts
@@ -0,0 +1,11 @@
+import { Category } from "./Category";
+
+export interface Product {
+    id : number;
+    name : string;
+    description : string;
+    price : number;
+    category : Category;
+    promotion : boolean;
+    newProduct : boolean;
+}
