d3601c1cba658f3d50bbc8463d77cfca1f19bd02
Rafael Martines <rafams_22@hotmail.com>
2024-07-11 22:26:03 -0300

    Botão Cadastrar PARTE-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { ProductsComponent } from "./components/products/products.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProductsComponent, FooterComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, ProductsComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Category } from '../../interfaces/Category';
 import { NgFor } from '@angular/common';
+import { Product } from '../../interfaces/Product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -14,9 +16,15 @@ export class ProductComponent implements OnInit{
  @Input()
   categories : Category [] = [];
 
+  product : Product = {} as Product;
+
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {    
   }
+
+  save() {
+    console.log(this.product);
+  }
+
 }
